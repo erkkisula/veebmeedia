5bff4d65cf681f4aee90a22b7ae2052bb4c2e047
Erkki Sula <43129445+erkkisula@users.noreply.github.com>
2020-02-18 23:51:58 +0200

    More Kanye

--- color.js
+++ color.js
@@ -1,10 +1,22 @@
 let block = document.getElementById('first');
+let isScreenFlashing = false;
 
 setInterval(function() {
     const r = Math.round(Math.random() * 255);
     const g = Math.round(Math.random() * 255);
     const b = Math.round(Math.random() * 255);
 
-    //document.body.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
     first.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+    if (isScreenFlashing) {
+        document.body.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+    }
 }, 1000);
+
+const colorClick = () => {
+    isScreenFlashing = !isScreenFlashing;
+    console.log(isScreenFlashing);
+};
+
+const resetColor = () => {
+    document.body.style.backgroundColor = '#f1f1f1';
+};
